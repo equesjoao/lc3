2f0e51edfb8389560b57500d4fd96532527d73f8
João R. <joaopedrorodrigueslc@gmail.com>
2024-12-30 08:09:22 -0300

    add: ADD implementation

--- lc3.c
+++ lc3.c
@@ -64,43 +64,6 @@
   
 };
 
-int main(int argc, char *argv[])
-{
-  if (argc < 2)
-  {
-    printf("usage: lc3 [img]\n");
-    exit(2);
-  } 
-
-  for (int i = 1; i < argc; ++i)
-  {
-    if (!readimg(argv[j])) {
-      printf("failed to load image: %s\n", argv[j]);
-        exit(1);
-    }
-  }
-
-  reg[COND] = ZRO;
-  enum { START = 0x3000 };
-  reg[RPC] = START;
-
-  int rung = 1;
-  while (rung) 
-  {
-    uint16_t instruc = mem_read(reg[RPC]++);
-    uint16_t opc = instruc >> 12;
-  }
-}
-
-
-uint16_t sign_extend(uint16_t x, int bitc)
-{
-  if ((x >> (bitc - 1)) & 1) {
-    x |= (0xFFFF << bitc); 
-  }  
-  return x;
-}
-
 void update_flags(uint16_t r)
 {
   if (reg[r] == 0) 
@@ -115,3 +78,68 @@ void update_flags(uint16_t r)
     reg[COND] = POS;
   }
 }
+
+uint16_t sign_extend(uint16_t x, int bitc)
+{
+  if ((x >> (bitc - 1)) & 1) {
+    x |= (0xFFFF << bitc); 
+  }  
+  return x;
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    printf("usage: lc3 [img]\n");
+    exit(2);
+  } 
+
+  for (int i = 1; i < argc; ++i)
+  {
+    if (!readimg(argv[i])) {
+      printf("failed to load image: %s\n", argv[i]);
+        exit(1);
+    }
+  }
+
+  reg[COND] = ZRO;
+  enum { START = 0x3000 };
+  reg[RPC] = START;
+
+  int run = 1;
+
+  while (run) 
+  {
+    uint16_t instruc = mem_read(reg[RPC]++);
+    uint16_t opc = instruc >> 12;
+
+    switch (opc) 
+    {
+      case OP_ADD:
+        // destination register (DR)
+        uint16_t r0 = (instruc >> 9) & 0x7;
+        // first operand (SR1)
+        uint16_t r1 = (instruc >> 6) & 0x7;
+
+        uint16_t imm = (instruc >> 5) & 0x1;
+
+        if (imm)
+        {
+          uint16_t imm5 = sign_extend(instruc & 0x1F, 5);
+          reg[r0] = reg[r1] + imm5;
+        }
+
+        else {
+          uint16_t r2 = instruc & 0x7;
+          reg[r0] = reg[r1] + reg[r2];
+        }
+
+        update_flags(r0);
+        break;
+    }
+  }
+}
+
+
+
